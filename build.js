9cdbde1b8486215a282f16389c1b71b21462008e
Documents <lucas@documents.design>
2020-11-24 09:54:59 +0100

    Updates build

--- build.js
+++ build.js
@@ -14,12 +14,23 @@ const toFrenchOption = o => ({
 }[o] || "");
 
 const writeHTML = students => {
+
     const students_string = students.map(s => {
+        const rotate = Math.random() * 180;
+        const dec1 = Math.random() > 0.5 ? -20 : 20;
+        const dec2 = Math.random() > 0.5 ? -20 : 20;
+        const top = Math.random() * dec1 + 50;
+        const left = Math.random() * dec2 + 50;
         return `
         <article>
+            ${s.image ? `<img class="insecte" src="images/${s.image}" style="top: ${top}%; left: ${left}%; transform: translate(-50%, -50%) rotate(${rotate}deg)"/>` : ''}
+            <div class="content">
             <h2>${s.prenom} ${s.nom}</h2>
             <h3>${s.annee} ${s.option}</h3>
-            <a href="${s.site}">${s.site.replace(/https?\:\/\//, '')}</a>
+            ${
+                s.site ? `<a href="${s.site}">${s.site.replace(/https?\:\/\//, '')}</a>` : s.pdf ? `<a href="pdf/${s.pdf}">Portfolio.pdf</a>` : ''
+            }
+            </div>
         </article>
         `;
     }).join('\n');
@@ -35,13 +46,15 @@ fs.readFile('liste_sites.csv', (err, data) => {
     const str = data.toString();
     const lines = str.split('\r\n');
     const students = lines.slice(1).map(a => {
-        const [prenom, nom, annee, option, site] = a.split(',');
+        const [prenom, nom, annee, option, site, pdf, image] = a.split(',');
         return {
             prenom,
             nom,
             annee: toFrenchAnnee(annee),
             option: toFrenchOption(option),
             site,
+            pdf,
+            image
         };
     });
     students.sort((a, b) => a.prenom.toLowerCase() > b.prenom.toLowerCase() ? 1 : -1);
